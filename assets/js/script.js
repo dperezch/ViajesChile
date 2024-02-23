90cf4fd450b7daecb476a3ebf183be4efa35f9ef
dperezch <davarmando@gmail.com>
2024-02-23 12:34:52 -0300

    Navbar: se agrega cambio de color de fondo al hacer scroll en la página usando JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,23 @@ $(document).ready(function () {
   const tooltipList = [...tooltipTriggerList].map(
     (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
   );
+
+  /* cambiar bg de navbar al hacer scroll  */
+  $(window).scroll(function () {
+    //scrollTop permite detectar el desplazamiento vertical de la páginas desde  la parte superior
+    var scrollTop = $(window).scrollTop();
+    //aplicar cambio solamente en pantalla grande
+    if (window.innerWidth > 768) {
+      if (scrollTop > 100) {
+        $(".navbar").css({
+          background: "rgba(0, 0, 0, 0.8)",
+          transition: "background 500ms ease",
+        });
+      } else
+        $(".navbar").css({
+          background: "transparent",
+          transition: "background 500ms ease",
+        });
+    }
+  });
 });
