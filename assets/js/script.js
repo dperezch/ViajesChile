a93442c76a8c6f3a2b9484bddc3fade9446998ef
david note <davarmando@gmail.com>
2024-02-22 22:37:29 -0300

    Section: Contacto - se añaden tooltips

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  /* Activar Popover Bootstrap */
+  const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+  );
+  const popoverList = [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+  );
+  /* Activar Tooltip Bootstrap */
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+});
